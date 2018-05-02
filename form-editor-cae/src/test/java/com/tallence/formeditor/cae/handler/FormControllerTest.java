6a415a2cb9ce4f6d2627217e87cd7a2b79355603
Timo Lemke <t.lemke@tallence.com>
2018-05-02 11:01:28 +0200

    #2 reformatting code in FormControllerTest

--- form-editor-cae/src/test/java/com/tallence/formeditor/cae/handler/FormControllerTest.java
+++ form-editor-cae/src/test/java/com/tallence/formeditor/cae/handler/FormControllerTest.java
@@ -44,115 +44,114 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 /**
  * Test for {@link FormController}
- *
  */
 @WebAppConfiguration
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = FormTestConfiguration.class)
 public class FormControllerTest {
 
-	@Autowired
- 	private WebApplicationContext context;
   @Autowired
- 	private StorageAdapterMock storageAdapterMock;
+  private WebApplicationContext context;
   @Autowired
- 	private MailAdapterMock mailAdapterMock;
+  private StorageAdapterMock storageAdapterMock;
+  @Autowired
+  private MailAdapterMock mailAdapterMock;
 
- 	private MockMvc mvc;
-	private static final URI TEST_URL = UriComponentsBuilder.fromUriString(PROCESS_SOCIAL_FORM).buildAndExpand("6", "2").toUri();
-	private static final String FORM_DATA_SERIALIZED = "TestName: 12345<br/>TestName: 18<br/>TestName: 12345<br/>TestName: null<br/>TestName: null<br/>TestName: [12345, ]<br/>TestName: []<br/>TestName: 12345<br/>TestName: null<br/>TestName: ist Text<br/>TestName: Das ist ein langer Text zur Erklärung des Formulars<br/>TestName: test@test.de<br/>";
+  private MockMvc mvc;
+  private static final URI TEST_URL = UriComponentsBuilder.fromUriString(PROCESS_SOCIAL_FORM).buildAndExpand("6", "2").toUri();
+  private static final String FORM_DATA_SERIALIZED = "TestName: 12345<br/>TestName: 18<br/>TestName: 12345<br/>TestName: null<br/>TestName: null<br/>TestName: [12345, ]<br/>TestName: []<br/>TestName: 12345<br/>TestName: null<br/>TestName: ist Text<br/>TestName: Das ist ein langer Text zur Erklärung des Formulars<br/>TestName: test@test.de<br/>";
 
-	@Before
- 	public void setup() {
- 		mvc = MockMvcBuilders
- 				.webAppContextSetup(context)
- 				.build();
- 	}
+  @Before
+  public void setup() {
+    mvc = MockMvcBuilders
+        .webAppContextSetup(context)
+        .build();
+  }
 
- 	@After
-	public void tearDown() {
- 		mailAdapterMock.clear();
- 		storageAdapterMock.clear();
-	}
+  @After
+  public void tearDown() {
+    mailAdapterMock.clear();
+    storageAdapterMock.clear();
+  }
 
 
   @Test
   public void testValidPost() throws Exception {
 
-		mvc.perform(fileUpload(TEST_URL)
-  				.param("TextField", "12345")
-					.param("NumberField", "18")
-					.param("RadioButtonsMandatory", "12345")
-					.param("CheckBoxesMandatory", "12345")
-					.param("SelectBoxMandatory", "12345")
-					.param("TextArea", "ist Text")
-					.param("UsersMail", "test@test.de")
-				)
-				.andExpect(status().is2xxSuccessful())
-				.andExpect(content().string("{\"success\":true,\"error\":null}"))
-				.andDo(MockMvcResultHandlers.print());
+    mvc.perform(fileUpload(TEST_URL)
+        .param("TextField", "12345")
+        .param("NumberField", "18")
+        .param("RadioButtonsMandatory", "12345")
+        .param("CheckBoxesMandatory", "12345")
+        .param("SelectBoxMandatory", "12345")
+        .param("TextArea", "ist Text")
+        .param("UsersMail", "test@test.de")
+    )
+        .andExpect(status().is2xxSuccessful())
+        .andExpect(content().string("{\"success\":true,\"error\":null}"))
+        .andDo(MockMvcResultHandlers.print());
 
-		assertEquals(storageAdapterMock.formData, FORM_DATA_SERIALIZED);
-		assertEquals(mailAdapterMock.usersFormData, FORM_DATA_SERIALIZED);
-		assertEquals(mailAdapterMock.usersRecipient, "test@test.de");
+    assertEquals(storageAdapterMock.formData, FORM_DATA_SERIALIZED);
+    assertEquals(mailAdapterMock.usersFormData, FORM_DATA_SERIALIZED);
+    assertEquals(mailAdapterMock.usersRecipient, "test@test.de");
 
-	}
+  }
 
   @Test
   public void testValidPostWithFile() throws Exception {
 
-		MockMultipartFile firstFile = new MockMultipartFile("FileUpload", "filename.txt", "text/plain", "some xml".getBytes());
+    MockMultipartFile firstFile = new MockMultipartFile("FileUpload", "filename.txt", "text/plain", "some xml".getBytes());
 
-		mvc.perform(fileUpload(TEST_URL)
-					.file(firstFile)
-  				.param("TextField", "12345")
-					.param("NumberField", "18")
-					.param("RadioButtonsMandatory", "12345")
-					.param("CheckBoxesMandatory", "12345")
-					.param("SelectBoxMandatory", "12345")
-					.param("TextArea", "ist Text")
-					.param("UsersMail", "test@test.de")
-				)
-				.andExpect(status().is2xxSuccessful())
-				.andExpect(content().string("{\"success\":true,\"error\":null}"))
-				.andDo(MockMvcResultHandlers.print());
+    mvc.perform(fileUpload(TEST_URL)
+        .file(firstFile)
+        .param("TextField", "12345")
+        .param("NumberField", "18")
+        .param("RadioButtonsMandatory", "12345")
+        .param("CheckBoxesMandatory", "12345")
+        .param("SelectBoxMandatory", "12345")
+        .param("TextArea", "ist Text")
+        .param("UsersMail", "test@test.de")
+    )
+        .andExpect(status().is2xxSuccessful())
+        .andExpect(content().string("{\"success\":true,\"error\":null}"))
+        .andDo(MockMvcResultHandlers.print());
 
-		assertEquals(storageAdapterMock.formData, FORM_DATA_SERIALIZED + "FileUpload: filename.txt<br/>");
-		assertEquals(mailAdapterMock.usersFormData, FORM_DATA_SERIALIZED + "FileUpload: filename.txt<br/>");
-		assertEquals(mailAdapterMock.usersRecipient, "test@test.de");
-	}
+    assertEquals(storageAdapterMock.formData, FORM_DATA_SERIALIZED + "FileUpload: filename.txt<br/>");
+    assertEquals(mailAdapterMock.usersFormData, FORM_DATA_SERIALIZED + "FileUpload: filename.txt<br/>");
+    assertEquals(mailAdapterMock.usersRecipient, "test@test.de");
+  }
 
   @Test
   public void testValidPostWithMailAction() throws Exception {
 
-		URI mailTestUrl = UriComponentsBuilder.fromUriString(PROCESS_SOCIAL_FORM).buildAndExpand("6", "4").toUri();
+    URI mailTestUrl = UriComponentsBuilder.fromUriString(PROCESS_SOCIAL_FORM).buildAndExpand("6", "4").toUri();
 
-		mvc.perform(post(mailTestUrl)
-					.param("TextArea", "ist Text")
-					.param("UsersMail", "test@test.de")
-				)
-				.andExpect(status().is2xxSuccessful())
-				.andExpect(content().string("{\"success\":true,\"error\":null}"))
-				.andDo(MockMvcResultHandlers.print());
+    mvc.perform(post(mailTestUrl)
+        .param("TextArea", "ist Text")
+        .param("UsersMail", "test@test.de")
+    )
+        .andExpect(status().is2xxSuccessful())
+        .andExpect(content().string("{\"success\":true,\"error\":null}"))
+        .andDo(MockMvcResultHandlers.print());
 
-		//check if the storageAdapter was not called
-		assertNull(storageAdapterMock.formData);
-		assertEquals(mailAdapterMock.adminFormData, "TestName: ist Text<br/>TestName: test@test.de<br/>");
-		assertEquals(mailAdapterMock.adminRecipient, "admin@test.de");
-		assertEquals(mailAdapterMock.usersFormData, "TestName: ist Text<br/>TestName: test@test.de<br/>");
-		assertEquals(mailAdapterMock.usersRecipient, "test@test.de");
-	}
+    //check if the storageAdapter was not called
+    assertNull(storageAdapterMock.formData);
+    assertEquals(mailAdapterMock.adminFormData, "TestName: ist Text<br/>TestName: test@test.de<br/>");
+    assertEquals(mailAdapterMock.adminRecipient, "admin@test.de");
+    assertEquals(mailAdapterMock.usersFormData, "TestName: ist Text<br/>TestName: test@test.de<br/>");
+    assertEquals(mailAdapterMock.usersRecipient, "test@test.de");
+  }
 
 
   @Test
   public void testInValidPost() throws Exception {
 
-		//Performing a post with only the TextField given will cause a validation error, because other mandatory fields are missing.
-		mvc.perform(fileUpload(TEST_URL).param("TextField", "12345"))
-				.andExpect(status().is4xxClientError())
-				.andExpect(content().string("{\"success\":false,\"error\":\"server-validation-failed\"}"))
-				.andDo(MockMvcResultHandlers.print());
+    //Performing a post with only the TextField given will cause a validation error, because other mandatory fields are missing.
+    mvc.perform(fileUpload(TEST_URL).param("TextField", "12345"))
+        .andExpect(status().is4xxClientError())
+        .andExpect(content().string("{\"success\":false,\"error\":\"server-validation-failed\"}"))
+        .andDo(MockMvcResultHandlers.print());
 
-	}
+  }
 
 }
