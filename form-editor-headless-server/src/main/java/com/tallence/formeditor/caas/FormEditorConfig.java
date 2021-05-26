9ff1d1932c856bc887d1a994303424fa3026cbfd
Philipp Hofmann <philipp.hofmann@coremedia.com>
2021-05-26 11:54:42 +0200

    Support for validators

--- form-editor-headless-server/src/main/java/com/tallence/formeditor/caas/FormEditorConfig.java
+++ form-editor-headless-server/src/main/java/com/tallence/formeditor/caas/FormEditorConfig.java
@@ -2,30 +2,12 @@ package com.tallence.formeditor.caas;
 
 import com.coremedia.caas.wiring.ProvidesTypeNameResolver;
 import com.coremedia.caas.wiring.TypeNameResolver;
-import com.coremedia.cap.content.ContentRepository;
-import com.coremedia.springframework.xml.ResourceAwareXmlBeanDefinitionReader;
 import com.tallence.formeditor.caas.adapter.FormEditorAdapterFactory;
 import com.tallence.formeditor.cae.FormElementFactory;
-import com.tallence.formeditor.cae.elements.CheckBoxesGroup;
-import com.tallence.formeditor.cae.elements.ComplexValue;
-import com.tallence.formeditor.cae.elements.ConsentFormCheckBox;
-import com.tallence.formeditor.cae.elements.DateField;
-import com.tallence.formeditor.cae.elements.FaxField;
-import com.tallence.formeditor.cae.elements.FileUpload;
 import com.tallence.formeditor.cae.elements.FormElement;
-import com.tallence.formeditor.cae.elements.NumberField;
-import com.tallence.formeditor.cae.elements.PhoneField;
-import com.tallence.formeditor.cae.elements.RadioButtonGroup;
-import com.tallence.formeditor.cae.elements.SelectBox;
-import com.tallence.formeditor.cae.elements.StreetNumberField;
-import com.tallence.formeditor.cae.elements.TextArea;
-import com.tallence.formeditor.cae.elements.TextField;
-import com.tallence.formeditor.cae.elements.TextOnly;
-import com.tallence.formeditor.cae.elements.UsersMail;
-import com.tallence.formeditor.cae.elements.ZipField;
+import com.tallence.formeditor.cae.validator.Validator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
 
 import java.util.Optional;
 import java.util.Set;
@@ -35,6 +17,22 @@ import java.util.stream.Stream;
 @Configuration(proxyBeanMethods = false)
 public class FormEditorConfig {
 
+  private static final Set<String> VALIDATORS = Stream.of(
+          "Validator",
+          "SizeValidator",
+          "NumberValidator",
+          "UsersMailValidator",
+          "TextValidator",
+          "TextOnlyValidator",
+          "SelectBoxValidator",
+          "RadioButtonGroupValidator",
+          "FileUploadValidator",
+          "DateFieldValidator",
+          "ConsentFormCheckboxValidator",
+          "CheckBoxesGroupValidator"
+
+  ).collect(Collectors.toSet());
+
   @Bean
   public FormEditorAdapterFactory formEditorAdapter(FormElementFactory formElementFactory) {
     return new FormEditorAdapterFactory(formElementFactory);
@@ -42,11 +40,19 @@ public class FormEditorConfig {
 
   @Bean
   public ProvidesTypeNameResolver providesFormBeanTypeNameResolver(FormElementFactory formElementFactory) {
-    return typeName ->
-            formElementFactory.getTypes().contains(typeName)
-                    || "FormElement".equals(typeName)
-                    || "RadioButtonGroup".equals(typeName) //The parsers key is different to the forms name
-                    || "CheckBoxesGroup".equals(typeName) //The parsers key is different to the forms name
+    return Name ->
+            formElementFactory.getTypes().contains(Name)
+                    || "FormElement".equals(Name)
+                    || "RadioButtonGroup".equals(Name) //The parsers key is different to the forms name
+                    || "CheckBoxesGroup".equals(Name) //The parsers key is different to the forms name
+                    ? Optional.of(true)
+                    : Optional.empty();
+  }
+
+  @Bean
+  public ProvidesTypeNameResolver providesFormValidatorTypeNameResolver(FormElementFactory formElementFactory) {
+    return Name ->
+            VALIDATORS.contains(Name)
                     ? Optional.of(true)
                     : Optional.empty();
   }
@@ -55,7 +61,15 @@ public class FormEditorConfig {
   public TypeNameResolver<FormElement> formElementTypeNameResolver() {
     return element -> {
       String simpleClassName = element.getClass().getSimpleName();
-      return Optional.of(simpleClassName+ "Impl");
+      return Optional.of(simpleClassName + "Impl");
+    };
+  }
+
+  @Bean
+  public TypeNameResolver<Validator> validatorTypeNameResolver() {
+    return element -> {
+      String simpleClassName = element.getClass().getSimpleName();
+      return Optional.of(simpleClassName);
     };
   }
 }
