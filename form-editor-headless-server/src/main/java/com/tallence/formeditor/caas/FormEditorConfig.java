e6538c230c66424c6b13edfcb4ec6b03aefe0eec
Timo Lemke <t.lemke@tallence.com>
2021-08-26 18:01:57 +0200

    Preparations for the headless-form-module: documentations

--- form-editor-headless-server/src/main/java/com/tallence/formeditor/caas/FormEditorConfig.java
+++ form-editor-headless-server/src/main/java/com/tallence/formeditor/caas/FormEditorConfig.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2018 Tallence AG
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.tallence.formeditor.caas;
 
 import com.coremedia.caas.wiring.ProvidesTypeNameResolver;
@@ -20,7 +35,12 @@ import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 /**
- * TODO this is still WIP! Do not use yet
+ * Configuration for the FormEditor Headless component.
+ * It is mapping the {@link FormElement}'s and {@link Validator}'s classes simpleNames to a GrapgQL-Schema-Element.
+ *
+ * Custom additional FormElements and Validators might be added to the classPath as well and will be handled if they are
+ * placed in the {@link com.tallence.formeditor.elements} or {@link com.tallence.formeditor.validator} packages.
+ * For elements and validators outside this package, you will have to define your own {@link ProvidesTypeNameResolver}
  */
 @Configuration(proxyBeanMethods = false)
 @Import(value = {MultiSiteConfiguration.class,})
